671fa0cb0bcfd0e4c45545a00c3acd5b83c1fbcb
Abhi Yerra <abhi@berkeley.edu>
2016-01-27 00:17:36 -0500

    Updated comments on meminfo

--- meminfo.go
+++ meminfo.go
@@ -28,10 +28,8 @@ type Meminfo struct {
 	// get tremendously large (20MB or so)
 	Buffers int64 `meminfo:"Buffers"`
 	Cached  int64 `meminfo:"Cached"`
-	// Memory that once was swapped out, is swapped back in but
-	// still also is in the swapfile (if memory is needed it
-	// doesn't need to be swapped out AGAIN because it is already
-	// in the swapfile. This saves I/O)
+	// SwapCached is the current amount of swap space used as a
+	// cache.
 	SwapCached int64 `meminfo:"SwapCached"`
 	// Memory that has been used more recently and usually not
 	// reclaimed unless absolutely necessary.
@@ -47,8 +45,7 @@ type Meminfo struct {
 	Mlocked      int64 `meminfo:"Mlocked"`
 	// total amount of swap space available
 	SwapTotal int64 `meminfo:"SwapTotal"`
-	// Memory which has been evicted from RAM, and is temporarily
-	// on the disk
+	// SwapFree is the total amount of swap space free.
 	SwapFree int64 `meminfo:"SwapFree"`
 	// Memory which is waiting to get written back to the disk
 	Dirty int64 `meminfo:"Dirty"`

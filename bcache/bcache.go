7df2ccaafe8e09771b091dfc3feff0d45419229a
Roger Luethi <rl@patchworkscience.org>
2017-05-31 12:35:24 +0200

    Initial commit for adding bcache

---
+++ bcache/bcache.go
@@ -0,0 +1,81 @@
+// Copyright 2017 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bcache provides access to statistics exposed by the bcache (Linux
+// block cache).
+package bcache
+
+// Stats contains bcache runtime statistics, parsed from /sys/fs/bcache/.
+//
+// The names and meanings of each statistic were taken from bcache.txt and
+// files in drivers/md/bcache in the Linux kernel source. Counters are float64
+// (in-kernel counters are mostly unsigned long).
+type Stats struct {
+	// The name of the bcache used to source these statistics.
+	Name   string
+	Bcache bcacheStats
+	Bdevs  []bdevStats
+	Caches []cacheStats
+}
+
+// bcacheStats contains statistics tied to a bcache ID.
+type bcacheStats struct {
+	AverageKeySize        float64
+	BtreeCacheSize        float64
+	CacheAvailablePercent float64
+	Congested             float64
+	RootUsagePercent      float64
+	TreeDepth             float64
+	Internal              internalStats
+	FiveMin               PeriodStats
+	Total                 PeriodStats
+}
+
+type bdevStats struct {
+	Name      string
+	DirtyData float64
+	FiveMin   PeriodStats
+	Total     PeriodStats
+}
+
+type cacheStats struct {
+	Name            string
+	IOErrors        float64
+	MetadataWritten float64
+	Written         float64
+	Priority        priorityStats
+}
+
+type priorityStats struct {
+	UnusedPercent   float64
+	MetadataPercent float64
+}
+
+// InternalStats contains internal bcache statistics.
+type internalStats struct {
+	ActiveJournalEntries                float64
+	BtreeNodes                          float64
+	BtreeReadAverageDurationNanoSeconds float64
+	CacheReadRaces                      float64
+}
+
+// PeriodStats contains statistics for a time period (5 min or total).
+type PeriodStats struct {
+	Bypassed            float64
+	CacheBypassHits     float64
+	CacheBypassMisses   float64
+	CacheHits           float64
+	CacheMissCollisions float64
+	CacheMisses         float64
+	CacheReadaheads     float64
+}
---
+++ bcache/get_test.go
@@ -0,0 +1,101 @@
+// Copyright 2017 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bcache
+
+import (
+	"testing"
+	"math"
+)
+
+func TestParseStats(t *testing.T) {
+	// test dehumanize
+	dehumanizeTests := []struct {
+		in  []byte
+		out float64
+	}{
+		{
+			in:  []byte("542k"),
+			out: float64(555008),
+		},
+		{
+			in:  []byte("322M"),
+			out: float64(337641472),
+		},
+		{
+			in:  []byte("1.1k"),
+			out: float64(1124),
+		},
+		{
+			in:  []byte("1.9k"),
+			out: float64(1924),
+		},
+		{
+			in:  []byte("1.10k"),
+			out: float64(2024),
+		},
+	}
+	for _, tst := range dehumanizeTests {
+		got := dehumanize(tst.in)
+		if got != tst.out {
+			t.Errorf("dehumanize: %s, want %f, got %f", tst.in, tst.out, got)
+		}
+	}
+
+	// test parsePseudoFloat
+	parsePseudoFloatTests := []struct {
+		in  string
+		out float64
+	}{
+		{
+			in:  "1.1",
+			out: float64(1.097656),
+		},
+		{
+			in:  "1.9",
+			out: float64(1.878906),
+		},
+		{
+			in:  "1.10",
+			out: float64(1.976562),
+		},
+	}
+	for _, tst := range parsePseudoFloatTests {
+		got := parsePseudoFloat(tst.in)
+		if math.Abs(got - tst.out) > 0.0001 {
+			t.Errorf("parsePseudoFloat: %s, want %f, got %f", tst.in, tst.out, got)
+		}
+	}
+
+	// test priorityStats
+	var want = priorityStats{
+		UnusedPercent: 99,
+		MetadataPercent: 5,
+	}
+	var (
+		in string
+		gotErr error
+		got priorityStats
+	)
+	in = "Metadata:       5%"
+	gotErr = parsePriorityStats(in, &got)
+	if gotErr != nil || got.MetadataPercent != want.MetadataPercent {
+		t.Errorf("parsePriorityStats: '%s', want %f, got %f", in, want.MetadataPercent, got.MetadataPercent)
+	}
+
+	in = "Unused:         99%"
+	gotErr = parsePriorityStats(in, &got)
+	if gotErr != nil || got.UnusedPercent != want.UnusedPercent {
+		t.Errorf("parsePriorityStats: '%s', want %f, got %f", in, want.UnusedPercent, got.UnusedPercent)
+	}
+}

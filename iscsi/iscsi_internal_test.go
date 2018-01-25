a946aacd58c325356986c8921ea7c17f069dfdc5
Alex Lau (AvengerMoJo) <alau@suse.com>
2018-01-25 23:18:59 +0800

    Fix fixtures typo and add ReadWriteIops test case
    Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

--- iscsi/iscsi_internal_test.go
+++ iscsi/iscsi_internal_test.go
@@ -14,25 +14,28 @@
 package iscsi
 
 import (
-	"reflect"
+//	"reflect"
 	"testing"
-    "path/filepath"
+
+    "github.com/prometheus/procfs/sysfs"
+    "github.com/prometheus/procfs/iscsi"
+    "github.com/kr/pretty"
 )
 
 func TestGetStats(t *testing.T) {
 	tests := []struct {
 		invalid bool
-		stat    *Stats
+		stat    *iscsi.Stats
 	}{
 		{
-			stat: &Stats{
+			stat: &iscsi.Stats{
 				Name: "iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.8888bbbbddd0",
-				Tpgt: []TPGT{
+				Tpgt: []iscsi.TPGT{
 					{
 						Name:     "tpgt_1",
 						TpgtPath: "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.8888bbbbddd0/tpgt_1",
 						IsEnable: true,
-						Luns: []LUN{
+						Luns: []iscsi.LUN{
 							{
 								Name:       "lun_0",
 								LunPath:    "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.8888bbbbddd0/tpgt_1/lun/lun_0",
@@ -46,14 +49,14 @@ func TestGetStats(t *testing.T) {
 			},
 		},
 		{
-			stat: &Stats{
+			stat: &iscsi.Stats{
 				Name: "iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.abcd1abcd2ab",
-				Tpgt: []TPGT{
+				Tpgt: []iscsi.TPGT{
 					{
 						Name:     "tpgt_1",
 						TpgtPath: "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.abcd1abcd2ab/tpgt_1",
 						IsEnable: true,
-						Luns: []LUN{
+						Luns: []iscsi.LUN{
 							{
 								Name:       "lun_0",
 								LunPath:    "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2003-01.org.linux-iscsi.osd1.x8664:sn.abcd1abcd2ab/tpgt_1/lun/lun_0",
@@ -67,14 +70,14 @@ func TestGetStats(t *testing.T) {
 			},
 		},
 		{
-			stat: &Stats{
+			stat: &iscsi.Stats{
 				Name: "iqn.2016-11.org.linux-iscsi.igw.x86:dev.rbd0",
-				Tpgt: []TPGT{
+				Tpgt: []iscsi.TPGT{
 					{
 						Name:     "tpgt_1",
 						TpgtPath: "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2016-11.org.linux-iscsi.igw.x86:dev.rbd0/tpgt_1",
 						IsEnable: true,
-						Luns: []LUN{
+						Luns: []iscsi.LUN{
 							{
 								Name:       "lun_0",
 								LunPath:    "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2016-11.org.linux-iscsi.igw.x86:dev.rbd0/tpgt_1/lun/lun_0",
@@ -88,14 +91,14 @@ func TestGetStats(t *testing.T) {
 			},
 		},
 		{
-			stat: &Stats{
+			stat: &iscsi.Stats{
 				Name: "iqn.2016-11.org.linux-iscsi.igw.x86:sn.ramdemo",
-				Tpgt: []TPGT{
+				Tpgt: []iscsi.TPGT{
 					{
 						Name:     "tpgt_1",
 						TpgtPath: "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2016-11.org.linux-iscsi.igw.x86:sn.ramdemo/tpgt_1",
 						IsEnable: true,
-						Luns: []LUN{
+						Luns: []iscsi.LUN{
 							{
 								Name:       "lun_0",
 								LunPath:    "../sysfs/fixtures/kernel/config/target/iscsi/iqn.2016-11.org.linux-iscsi.igw.x86:sn.ramdemo/tpgt_1/lun/lun_0",
@@ -110,25 +113,53 @@ func TestGetStats(t *testing.T) {
 		},
 	}
 
-    SetPath("../sysfs/fixtures")
-    matches, err := filepath.Glob(filepath.Join(sysPath, TARGETPATH, "/iqn*"))
+    readTests := []struct {
+        read uint64
+        write uint64
+        iops uint64
+    }{
+        { 10325, 40325, 204950 },
+        { 20095, 71235, 104950 },
+        { 10195, 30195, 301950 },
+        { 1504 , 4733 , 1234   }, 
+    }
+
+    sysfsStat, err := sysfs.FS("../sysfs/fixtures").ISCSIStats()
 	if err != nil {
 		t.Errorf("unexpected test fixtures")
 	}
-    fstats := make([]*Stats, 0, len(matches))
-    for _, iqnPath := range matches {
-        name := filepath.Base(iqnPath)
-        s, err := GetStats(iqnPath)
-        if err != nil {
-			t.Errorf("unexpected iSCSI stats:%s", iqnPath)
-        }
-        s.Name = name
-        fstats = append(fstats, s)
-    }
 
-	for i, stat := range fstats {
-		if want, have := tests[i].stat, stat; !reflect.DeepEqual(want, have) {
-			t.Errorf("unexpected iSCSI stats:\nwant:\n%v\nhave:\n%v", want, have)
-		}
+	for i, stat := range sysfsStat {
+		want, have := tests[i].stat, stat;
+        diff := pretty.Diff(want, have)
+        if diff != nil {
+			t.Errorf("unexpected iSCSI stats:\ndiff:\n%v", diff)
+			t.Errorf("\nwant:\n%v\nhave:\n%v", want, have)
+        } else {
+            readMB, writeMB, iops, err := iscsi.ReadWriteOPS(stat.Name,
+            stat.Tpgt[0].Name, stat.Tpgt[0].Luns[0].Name )
+            if err != nil { 
+			    t.Errorf("unexpected iSCSI ReadWriteOPS path %s %s %s",
+                stat.Name, stat.Tpgt[0].Name, stat.Tpgt[0].Luns[0].Name)
+			    t.Errorf("%v", err);
+            }
+            // datawant, datahave := readTests[i], { readMB, writeMB, iops };
+
+            diff = pretty.Diff(readTests[i].read, readMB)
+            if diff != nil {
+                t.Errorf("unexpected iSCSI read data :\ndiff:\n%v", diff)
+                t.Errorf("\nwant:\n%v\nhave:\n%v", readTests[i].read, readMB)
+            }
+            diff = pretty.Diff(readTests[i].write, writeMB)
+            if diff != nil {
+                t.Errorf("unexpected iSCSI write data :\ndiff:\n%v", diff)
+                t.Errorf("\nwant:\n%v\nhave:\n%v", readTests[i].write, writeMB)
+            }
+            diff = pretty.Diff(readTests[i].iops, iops)
+            if diff != nil {
+                t.Errorf("unexpected iSCSI data :\ndiff:\n%v", diff)
+                t.Errorf("\nwant:\n%v\nhave:\n%v", readTests[i].iops, iops)
+            }
+        }
 	}
 }

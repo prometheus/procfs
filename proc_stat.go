3d0356c06deffbf3c63267b65e278b1a930bb79f
Peter Löffler <peter.loeffler@runtastic.com>
2018-09-27 13:18:54 +0200

    fix error handling
    Signed-off-by: Peter Löffler <peter.loeffler@runtastic.com>

--- proc_stat.go
+++ proc_stat.go
@@ -134,10 +134,14 @@ func (p Proc) NewStat() (ProcStat, error) {
 	for scanner.Scan() {
 		if bytes.Contains(scanner.Bytes(), []byte("Max open files")) {
 			mof, err = strconv.Atoi(strings.Fields(scanner.Text())[3])
-			if err := scanner.Err(); err != nil {
+			if err != nil {
 				return ProcStat{}, err
 			}
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return ProcStat{}, err
 	}
 
 	// count files in fd directory

ebdc8ebb149a43867a5a1e9277de5376c7a53d18
Paul Gier <pgier@redhat.com>
2019-09-11 16:48:10 -0500

    additional error handling when reading files in /sys/class
    Some regular files in /sys/class are not readable on certain
    systems, so just ignore them.
    
    Signed-off-by: Paul Gier <pgier@redhat.com>

--- sysfs/class_infiniband.go
+++ sysfs/class_infiniband.go
@@ -251,6 +251,9 @@ func parseInfiniBandCounters(portPath string) (*InfiniBandCounters, error) {
 		name := filepath.Join(path, f.Name())
 		value, err := util.SysReadFile(name)
 		if err != nil {
+			if os.IsNotExist(err) || err.Error() == "operation not supported" || err.Error() == "invalid argument" {
+				continue
+			}
 			return nil, fmt.Errorf("failed to read file %q: %v", name, err)
 		}
 
@@ -331,6 +334,9 @@ func parseInfiniBandCounters(portPath string) (*InfiniBandCounters, error) {
 		name := filepath.Join(path, f.Name())
 		value, err := util.SysReadFile(name)
 		if err != nil {
+			if os.IsNotExist(err) || err.Error() == "operation not supported" || err.Error() == "invalid argument" {
+				continue
+			}
 			return nil, fmt.Errorf("failed to read file %q: %v", name, err)
 		}
 

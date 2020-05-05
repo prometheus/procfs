2e4c1a585cf4986ca583e7522224f8d3c2b65f80
Paul Gier <pgier@redhat.com>
2020-05-05 06:58:26 -0500

    Add multiplatform support for cpuinfo (#257)
    * multiplatform support for cpuinfo
    
    This adds basic handling of reading /proc/cpuinfo for ARM, PPC, and
    s390x.
    
    Signed-off-by: Paul Gier <pgier@redhat.com>
    
    * use build constraints to detect current platform
    
    Use go build contraints to link the appropriate parsing function
    for the each arch.
    
    Signed-off-by: Paul Gier <pgier@redhat.com>
    
    * remove cpuinfo format detection
    
    CPU detection is now done using compiler tags, so the manual
    detection is no longer needed.
    
    Signed-off-by: Paul Gier <pgier@redhat.com>

--- cpuinfo_test.go
+++ cpuinfo_test.go
@@ -11,11 +11,106 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build linux
+
 package procfs
 
 import "testing"
 
-func TestCPUInfo(t *testing.T) {
+const (
+	cpuinfoArm7 = `
+Processor : ARMv7 Processor rev 5 (v7l)
+processor : 0
+BogoMIPS : 2400.00
+
+processor : 1
+BogoMIPS : 2400.00
+
+Features : swp half thumb fastmult vfp edsp thumbee neon vfpv3 tls vfpv4 idiva idivt
+CPU implementer : 0x41
+CPU architecture: 7
+CPU variant : 0x0
+CPU part : 0xc07
+CPU revision : 5
+
+Hardware : sun8i
+Revision : 0000
+Serial : 5400503583203c3c040e`
+
+	cpuinfoS390x = `
+vendor_id       : IBM/S390
+# processors    : 4
+bogomips per cpu: 3033.00
+max thread id   : 0
+features	: esan3 zarch stfle msa ldisp eimm dfp edat etf3eh highgprs te vx sie
+facilities      : 0 1 2 3 4 6 7 8 9 10 12 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 30 31 32 33 34 35 36 37 40 41 42 43 44 45 46 47 48 49 50 51 52 53 55 57 73 74 75 76 77 80 81 82 128 129 131
+cache0          : level=1 type=Data scope=Private size=128K line_size=256 associativity=8
+cache1          : level=1 type=Instruction scope=Private size=96K line_size=256 associativity=6
+cache2          : level=2 type=Data scope=Private size=2048K line_size=256 associativity=8
+cache3          : level=2 type=Instruction scope=Private size=2048K line_size=256 associativity=8
+cache4          : level=3 type=Unified scope=Shared size=65536K line_size=256 associativity=16
+cache5          : level=4 type=Unified scope=Shared size=491520K line_size=256 associativity=30
+processor 0: version = FF,  identification = 2733E8,  machine = 2964
+processor 1: version = FF,  identification = 2733E8,  machine = 2964
+processor 2: version = FF,  identification = 2733E8,  machine = 2964
+processor 3: version = FF,  identification = 2733E8,  machine = 2964
+
+cpu number      : 0
+cpu MHz dynamic : 5000
+cpu MHz static  : 5000
+
+cpu number      : 1
+cpu MHz dynamic : 5000
+cpu MHz static  : 5000
+
+cpu number      : 2
+cpu MHz dynamic : 5000
+cpu MHz static  : 5000
+
+cpu number      : 3
+cpu MHz dynamic : 5000
+cpu MHz static  : 5000
+`
+
+	cpuinfoPpc64 = `
+processor	: 0
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+processor	: 1
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+processor	: 2
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+processor	: 3
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+processor	: 4
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+processor	: 5
+cpu		: POWER7 (architected), altivec supported
+clock		: 3000.000000MHz
+revision	: 2.1 (pvr 003f 0201)
+
+timebase	: 512000000
+platform	: pSeries
+model		: IBM,8233-E8B
+machine		: CHRP IBM,8233-E8B
+`
+)
+
+func TestCPUInfoX86(t *testing.T) {
 	cpuinfo, err := getProcFixtures(t).CPUInfo()
 	if err != nil {
 		t.Fatal(err)
@@ -56,5 +151,52 @@ func TestCPUInfo(t *testing.T) {
 	if want, have := "vme", cpuinfo[7].Flags[1]; want != have {
 		t.Errorf("want flag %v, have %v", want, have)
 	}
+}
 
+func TestCPUInfoParseARM(t *testing.T) {
+	cpuinfo, err := parseCPUInfoARM([]byte(cpuinfoArm7))
+	if err != nil || cpuinfo == nil {
+		t.Fatalf("unable to parse arm cpu info: %v", err)
+	}
+	if want, have := 2, len(cpuinfo); want != have {
+		t.Errorf("want number of processors %v, have %v", want, have)
+	}
+	if want, have := "ARMv7 Processor rev 5 (v7l)", cpuinfo[0].VendorID; want != have {
+		t.Errorf("want vendor %v, have %v", want, have)
+	}
+	if want, have := "thumb", cpuinfo[1].Flags[2]; want != have {
+		t.Errorf("want flag %v, have %v", want, have)
+	}
+}
+
+func TestCPUInfoParseS390X(t *testing.T) {
+	cpuinfo, err := parseCPUInfoS390X([]byte(cpuinfoS390x))
+	if err != nil || cpuinfo == nil {
+		t.Fatalf("unable to parse s390x cpu info: %v", err)
+	}
+	if want, have := 4, len(cpuinfo); want != have {
+		t.Errorf("want number of processors %v, have %v", want, have)
+	}
+	if want, have := "IBM/S390", cpuinfo[0].VendorID; want != have {
+		t.Errorf("want vendor %v, have %v", want, have)
+	}
+	if want, have := "ldisp", cpuinfo[1].Flags[4]; want != have {
+		t.Errorf("want flag %v, have %v", want, have)
+	}
+	if want, have := 5000.0, cpuinfo[2].CPUMHz; want != have {
+		t.Errorf("want cpu MHz %v, have %v", want, have)
+	}
+}
+
+func TestCPUInfoParsePPC(t *testing.T) {
+	cpuinfo, err := parseCPUInfoPPC([]byte(cpuinfoPpc64))
+	if err != nil || cpuinfo == nil {
+		t.Fatalf("unable to parse ppc cpu info: %v", err)
+	}
+	if want, have := 6, len(cpuinfo); want != have {
+		t.Errorf("want number of processors %v, have %v", want, have)
+	}
+	if want, have := 3000.00, cpuinfo[2].CPUMHz; want != have {
+		t.Errorf("want cpu mhz %v, have %v", want, have)
+	}
 }

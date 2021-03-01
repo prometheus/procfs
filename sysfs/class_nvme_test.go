a4634b240886b96742da88c6d4d3e3d0c2919c28
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2021-03-01 13:10:19 +0100

    Add support for NVMe
    The sysfs contains some useful information about NVMe devices that
    should be exporter by the Prometheus node exporter. Add a `NVMeClass`
    function to collect the information for the node exporter.
    
    See: https://github.com/prometheus/node_exporter/issues/1891
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>

---
+++ sysfs/class_nvme_test.go
@@ -0,0 +1,48 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows
+
+package sysfs
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestNVMeClass(t *testing.T) {
+	fs, err := NewFS(sysTestFixtures)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := fs.NVMeClass()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := NVMeClass{
+		"nvme0": NVMeDevice{
+			Name:             "nvme0",
+			FirmwareRevision: "1B2QEXP7",
+			Model:            "Samsung SSD 970 PRO 512GB",
+			Serial:           "S680HF8N190894I",
+			State:            "live",
+		},
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("unexpected NVMe class (-want +got):\n%s", diff)
+	}
+}

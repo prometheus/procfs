1a7a2bd3279f8809ed8c293299b889fa30baae4b
Benjamin Drung <benjamin.drung@ionos.com>
2021-08-27 09:35:31 +0200

    Add support for DMI information
    The sysfs contains some useful information about the BIOS, board,
    chassis, product, and system from the Desktop Management Interface
    (DMI). The BIOS version should be exported by the Prometheus node
    exporter to support alerting about outdated BIOS versions. Add a
    `DMIClass` function to collect the information for the node exporter.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@ionos.com>

---
+++ sysfs/class_dmi_test.go
@@ -0,0 +1,80 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build linux
+
+package sysfs
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestDMIClass(t *testing.T) {
+	fs, err := NewFS(sysTestFixtures)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got, err := fs.DMIClass()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	empty := ""
+	biosDate := "04/12/2021"
+	biosRelease := "2.2"
+	biosVendor := "Dell Inc."
+	biosVersion := "2.2.4"
+	boardName := "07PXPY"
+	boardSerial := ".7N62AI2.GRTCL6944100GP."
+	boardVendor := "Dell Inc."
+	boardVersion := "A01"
+	chassisSerial := "7N62AI2"
+	chassisType := "23"
+	chassisVendor := "Dell Inc."
+	productFamily := "PowerEdge"
+	productName := "PowerEdge R6515"
+	productSerial := "7N62AI2"
+	productSKU := "SKU=NotProvided;ModelName=PowerEdge R6515"
+	productUUID := "83340ca8-cb49-4474-8c29-d2088ca84dd9"
+	systemVendor := "Dell Inc."
+
+	want := &DMIClass{
+		BiosDate:        &biosDate,
+		BiosRelease:     &biosRelease,
+		BiosVendor:      &biosVendor,
+		BiosVersion:     &biosVersion,
+		BoardName:       &boardName,
+		BoardSerial:     &boardSerial,
+		BoardVendor:     &boardVendor,
+		BoardVersion:    &boardVersion,
+		ChassisAssetTag: &empty,
+		ChassisSerial:   &chassisSerial,
+		ChassisType:     &chassisType,
+		ChassisVendor:   &chassisVendor,
+		ChassisVersion:  &empty,
+		ProductFamily:   &productFamily,
+		ProductName:     &productName,
+		ProductSerial:   &productSerial,
+		ProductSKU:      &productSKU,
+		ProductUUID:     &productUUID,
+		ProductVersion:  &empty,
+		SystemVendor:    &systemVendor,
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("unexpected DMI class (-want +got):\n%s", diff)
+	}
+}

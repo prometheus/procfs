2bd8cc9eaef77f13d4ddc91b3b3a9c715b648b3a
fangwentong <fangwentong2012@gmail.com>
2024-03-29 13:27:44 +0800

    Update build constraints for loong64 and s390x
    Signed-off-by: fangwentong <fangwentong2012@gmail.com>

--- cpuinfo_loong64.go
+++ cpuinfo_loong64.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build linux
-// +build linux
+//go:build linux && loong64
+// +build linux,loong64
 
 package procfs
 

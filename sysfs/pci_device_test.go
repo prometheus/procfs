22dbb891b7c8c22b2de0d55e5c1ef41d1a61b736
Jain Johny <jj@asama.ai>
2025-09-18 17:14:55 +0530

    support for sriov, numa and power management config
    Signed-off-by: Jain Johny <jj@asama.ai>

--- sysfs/pci_device_test.go
+++ sysfs/pci_device_test.go
@@ -34,9 +34,25 @@ func TestPciDevices(t *testing.T) {
 	}
 
 	var (
-		LinkSpeed8GTs = 8.0
-		LinkWidth4    = 4.0
-		LinkWidth8    = 8.0
+		LinkSpeed8GTs  = 8.0
+		LinkSpeed16GTs = 16.0
+		LinkWidth4     = 4.0
+		LinkWidth8     = 8.0
+
+		// SR-IOV test values
+		SriovDriversAutoprobe = true
+		SriovNumvfs           = 0
+		SriovOffset           = 8
+		SriovStride           = 1
+		SriovTotalvfs         = 128
+		SriovVfDevice         = 1889
+		SriovVfTotalMsix      = uint64(4294967033)
+
+		// Optional device test values
+		NumaNode      = int32(1)
+		NumaNodeNeg1  = int32(-1)
+		D3coldAllowed = true
+		PowerState    = PciPowerStateD0
 	)
 	want := PciDevices{
 		"0000:00:02:1": PciDevice{
@@ -54,11 +70,15 @@ func TestPciDevices(t *testing.T) {
 			SubsystemVendor: 0x17aa,
 			SubsystemDevice: 0x5095,
 			Revision:        0x00,
+			NumaNode:        &NumaNodeNeg1,
 
 			MaxLinkSpeed:     &LinkSpeed8GTs,
 			MaxLinkWidth:     &LinkWidth8,
 			CurrentLinkSpeed: &LinkSpeed8GTs,
 			CurrentLinkWidth: &LinkWidth4,
+
+			D3coldAllowed: &D3coldAllowed,
+			PowerState:    &PowerState,
 		},
 		"0000:01:00:0": PciDevice{
 			Location: PciDeviceLocation{
@@ -80,11 +100,50 @@ func TestPciDevices(t *testing.T) {
 			SubsystemVendor: 0xc0a9,
 			SubsystemDevice: 0x5021,
 			Revision:        0x01,
+			NumaNode:        &NumaNodeNeg1,
 
 			MaxLinkSpeed:     &LinkSpeed8GTs,
 			MaxLinkWidth:     &LinkWidth4,
 			CurrentLinkSpeed: &LinkSpeed8GTs,
 			CurrentLinkWidth: &LinkWidth4,
+
+			D3coldAllowed: &D3coldAllowed,
+			PowerState:    &PowerState,
+		},
+		"0000:a2:00:0": PciDevice{
+			Location: PciDeviceLocation{
+				Segment:  0,
+				Bus:      0xa2,
+				Device:   0,
+				Function: 0,
+			},
+			ParentLocation: nil,
+
+			Class:           0x020000,
+			Vendor:          0x8086,
+			Device:          0x159b,
+			SubsystemVendor: 0x8086,
+			SubsystemDevice: 0x0003,
+			Revision:        0x02,
+			NumaNode:        &NumaNode,
+
+			MaxLinkSpeed:     &LinkSpeed16GTs,
+			MaxLinkWidth:     &LinkWidth8,
+			CurrentLinkSpeed: &LinkSpeed16GTs,
+			CurrentLinkWidth: &LinkWidth8,
+
+			// SR-IOV fields
+			SriovDriversAutoprobe: &SriovDriversAutoprobe,
+			SriovNumvfs:           &SriovNumvfs,
+			SriovOffset:           &SriovOffset,
+			SriovStride:           &SriovStride,
+			SriovTotalvfs:         &SriovTotalvfs,
+			SriovVfDevice:         &SriovVfDevice,
+			SriovVfTotalMsix:      &SriovVfTotalMsix,
+
+			// Power management fields
+			D3coldAllowed: &D3coldAllowed,
+			PowerState:    &PowerState,
 		},
 	}
 

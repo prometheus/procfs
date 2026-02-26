50740d491dec4aac8e3e0866e2c55e75bdab7cf5
chijiajian <33536362+chijiajian@users.noreply.github.com>
2026-02-26 15:16:42 +0800

    Improve error handling in parseFibreChannelStatistics
    Refactor error handling for statistics directory check.
    
    Signed-off-by: chijiajian <33536362+chijiajian@users.noreply.github.com>

--- sysfs/class_fibrechannel.go
+++ sysfs/class_fibrechannel.go
@@ -152,9 +152,15 @@ func parseFibreChannelStatistics(hostPath string) (*FibreChannelCounters, error)
 	var counters FibreChannelCounters
 
 	path := filepath.Join(hostPath, "statistics")
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return &counters, nil
-	}
+	
+   if _, err := os.Stat(path); err != nil {
+      if errors.Is(err, fs.ErrNotExist) {
+        // statistics directory not present, skip
+          return &counters, nil
+      }
+      return nil, err
+    }
+	
 	files, err := os.ReadDir(path)
 	if err != nil {
 		return nil, err
